b732f75b5420745ff831f236957892ca3e947971
alar12 <adithyaavuthu@gmail.com>
2024-02-07 09:55:19 -0600

    GanapatayeNamaha

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,38 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'standard-with-typescript',
-    ],
-    overrides: [
-      {
-        env: {
-          node: true,
-        },
-        files: [
-          '.eslintrc.js',
-          '.eslintrc.cjs',
-        ],
-        parserOptions: {
-          sourceType: 'script',
-        },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
       },
-    ],
-    parserOptions: {
-      ecmaVersion: 2023,
-      sourceType: 'module',
-      project: './tsconfig.json',
-    },
-    plugins: ['react'],
-    rules: {
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  };
-  
+      files: [
+        '.eslintrc.js',
+        '.eslintrc.cjs'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 2023,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react'],
+  rules: {
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
