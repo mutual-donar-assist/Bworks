bfc58ad09d602366b0a1bae118fc5980d0e4a711
alar12 <adithyaavuthu@gmail.com>
2024-02-07 01:47:25 -0600

    ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
                 ".eslintrc.{js,cjs}"
             ],
             "parserOptions": {
+                project: './tsconfig.json',
                 "sourceType": "script"
             }
         }
