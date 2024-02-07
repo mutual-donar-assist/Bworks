6547e55bac26d2cbc34f2ca04b8f7796518cb01a
alar12 <adithyaavuthu@gmail.com>
2024-02-07 09:50:53 -0600

    JaiShreeRam

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+      browser: true,
+      es2021: true,
     },
-    "extends": [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        "standard-with-typescript",
-        "plugin:react/recommended"
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'standard-with-typescript',
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: [
+          '.eslintrc.js',
+          '.eslintrc.cjs',
+        ],
+        parserOptions: {
+          sourceType: 'script',
+        },
+      },
     ],
-    "parserOptions": {
-        project: 'Bworks/tsconfig.json',
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+      ecmaVersion: 2023,
+      sourceType: 'module',
+      project: './tsconfig.json',
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+    plugins: ['react'],
+    rules: {
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
+  
